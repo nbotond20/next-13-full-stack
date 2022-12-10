8a2d9117d7d67291b9fea2100940c31b633804a6
Nuszpl Botond <80464629+nbotond20@users.noreply.github.com>
2022-12-10 17:49:24 +0100

    Small changes to test the new workflow (#2)

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,11 +4,8 @@ import React from 'react'
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
       <head />
+      <h1>New H1</h1>
       <body>{children}</body>
     </html>
   )
