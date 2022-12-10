390a2acd933b533bda2cfb7d2c0117a8690c7212
Nuszpl Botond <80464629+nbotond20@users.noreply.github.com>
2022-12-10 14:18:17 +0100

    Added export script

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-}
-
-module.exports = nextConfig
+    experimental: {
+        appDir: true,
+    },
+    images: {
+        loader: "akamai",
+        path: "",
+    },
+};
+module.exports = nextConfig;
