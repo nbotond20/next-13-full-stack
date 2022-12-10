4f6946be4557dbdf603bb5f924137f7cf1066cb0
Nuszpl Botond <nuszpl.botond@danubiusinfo.hu>
2022-12-10 17:36:17 +0100

    Trigger workflow

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <head />
+      <h1>New H1</h1>
       <body>{children}</body>
     </html>
   )
