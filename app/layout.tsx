95dad017091a3467aae2dd46527b82083888d99c
Nuszpl Botond <80464629+nbotond20@users.noreply.github.com>
2022-12-11 12:24:33 +0100

    Lint warning fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,14 +3,16 @@
 import React from 'react'
 
 import { Typography } from '@mui/material'
+import Head from 'next/head'
 
 import './globals.css'
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <head />
-      <Typography variant="h1">MUI H1</Typography>
+      <Head>
+        <Typography variant="h1">MUI H1</Typography>
+      </Head>
       <body>{children}</body>
     </html>
   )
