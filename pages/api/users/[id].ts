2f2a11b609e52bf00287b137c13f8c6db5ed8572
Nuszpl Botond <80464629+nbotond20@users.noreply.github.com>
2022-12-11 19:24:26 +0100

    Added Prisma and implemeted very basic User CRUD (#25)

---
+++ pages/api/users/[id].ts
@@ -0,0 +1,14 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+import { getUser } from '../../../lib/prisma/users'
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === 'GET') {
+    if (!req.query.id) {
+      return res.status(404).json({ message: 'No user found' })
+    }
+    const { user } = await getUser(Number(req.query.id))
+    return res.status(200).json(user)
+  }
+}
