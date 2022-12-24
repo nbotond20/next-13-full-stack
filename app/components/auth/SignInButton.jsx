1c4e75dcc800d0b595532f394d3f74962e5a7e80
Nuszpl Botond <80464629+nbotond20@users.noreply.github.com>
2022-12-24 20:34:32 +0100

    Removed images (#61)

--- app/components/auth/SignInButton.jsx
+++ app/components/auth/SignInButton.jsx
@@ -2,31 +2,25 @@
 
 import React from 'react'
 
-import { useSession, signIn } from 'next-auth/react'
-import Image from 'next/image'
+import { Box } from '@mui/material'
+import { useSession, signIn, signOut } from 'next-auth/react'
 
 const SignInButton = () => {
   const { data: session } = useSession()
 
   if (session) {
     return (
-      <div>
-        {session?.user?.image ? (
-          <Image src={session.user.image} alt={session.user.name} fill />
-        ) : (
+      <>
+        <Box sx={{ width: '100px', height: '100px', position: 'fixed', right: 0, top: 0 }}>
           <span>
             <svg fill="currentColor" viewBox="0 0 24 24">
               <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
             </svg>
           </span>
-        )}
-      </div>
+        </Box>
 
-      // <button
-      //   onClick={() => signOut()}
-      // >
-      //   Sign Out
-      // </button>
+        <button onClick={() => signOut()}>Sign Out</button>
+      </>
     )
   }
 
