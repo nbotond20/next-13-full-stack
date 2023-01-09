b917310fdaabfe7c2656632b6b1df94cac57b41e
Nuszpl Botond <80464629+nbotond20@users.noreply.github.com>
2023-01-09 18:29:43 +0100

    Added creadential login/register (#101)

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,6 +1,8 @@
 import prisma from '@lib/prisma/prismadb'
 import { PrismaAdapter } from '@next-auth/prisma-adapter'
+import { User } from '@prisma/client'
 import NextAuth from 'next-auth'
+import CredentialsProvider from 'next-auth/providers/credentials'
 import GoogleProvider from 'next-auth/providers/google'
 
 export default NextAuth({
@@ -10,6 +12,35 @@ export default NextAuth({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        email: { label: 'Email', type: 'text', placeholder: 'jsmith@example.com' },
+        password: { label: 'Password', type: 'password' },
+      },
+      async authorize(credentials) {
+        const { email, password } = credentials as {
+          email: string
+          password: string
+        }
+
+        const url = process.env.NEXTAUTH_URL + `/api/auth/login`
+
+        const res = await fetch(url, {
+          method: 'POST',
+          body: JSON.stringify({ email, password }),
+          headers: { 'Content-Type': 'application/json' },
+        })
+
+        const { user } = (await res.json()) as { user: User }
+
+        if (user) {
+          return user
+        }
+
+        return null
+      },
+    }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
   session: {
@@ -18,6 +49,6 @@ export default NextAuth({
   },
   // Use this instead of default login/logout pages
   pages: {
-    signIn: '/signin',
+    signIn: '/login',
   },
 })
