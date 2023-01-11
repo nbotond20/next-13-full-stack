a656ab9d0ccf6e141fce25dd8814ec7d4661577a
Oli200110 <tbouwt@inf.elte.hu>
2023-01-11 18:27:20 +0100

    [movieid] page fetches contents from API, basic details listed; still needs styling and further functions

---
+++ app/(site)/movies/[movieId]/MovieDetails.tsx
@@ -0,0 +1,126 @@
+'use client'
+
+import React, { useEffect, useState } from 'react'
+
+import Image from 'next/image'
+
+//import useSWR from 'swr'
+
+interface MovieDetails {
+  id: number
+  title: string
+  genres: {
+    id: number
+    name: string
+  }[]
+
+  original_title: string | null
+  overview: string | null
+  production_companies: {
+    name: string
+  }[]
+  release_date: string
+  revenue: number
+  poster_path: string | null
+  backdrop_path: string | null
+}
+
+interface MovieDetailsProps {
+  results: MovieDetails
+  imgURL: string
+  MOVIES_DB_API_URL: string
+  MOVIES_DB_API_KEY: string
+}
+
+/*const fetcher = (url: string) => fetch(url).then(res => res.json())
+
+function useMovieDetails(movieId: string, MOVIES_DB_API_URL: string, MOVIES_DB_API_KEY: string) {
+  const { data, error, isLoading } = useSWR(
+    `${MOVIES_DB_API_URL}/movie/${movieId}?api_key=${MOVIES_DB_API_KEY}`,
+    fetcher
+  )
+
+  return {
+    details: data?.results,
+    isLoading,
+    error,
+  } as { details: MovieDetails; isLoading: boolean; error: Error }
+}*/
+
+export const MovieDetails = ({ results, imgURL }: MovieDetailsProps) => {
+  const [movieDetailsData, setMovieDetailsData] = useState(results)
+
+  useEffect(() => {
+    return () => {
+      setMovieDetailsData(results)
+    }
+  }, [results])
+
+  const USDollar = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    maximumSignificantDigits: 3,
+  })
+
+  return (
+    <>
+      <Image
+        className={'rounded-lg'}
+        src={`${imgURL}/${movieDetailsData.poster_path}`}
+        alt={''}
+        width={500}
+        height={500}
+      ></Image>
+      <h2 className={'text-4xl font-extrabold dark:text-white'}>{movieDetailsData.title}</h2>
+      <div className="relative overflow-x-auto shadow-md sm:rounded-lg max-w-2xl">
+        <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+          <tbody>
+            <tr className="border-b border-gray-200 dark:border-gray-700">
+              <th scope="row" className="px-6 py-3 bg-gray-50 dark:bg-gray-800">
+                Movie Title:
+              </th>
+              <td className="px-6 py-4">{movieDetailsData.title}</td>
+            </tr>
+            <tr className="border-b border-gray-200 dark:border-gray-700">
+              <th scope="row" className="px-6 py-3 bg-gray-50 dark:bg-gray-800">
+                Genres:
+              </th>
+              <td className="px-6 py-4">
+                {movieDetailsData.genres[0].name}
+                {movieDetailsData.genres[1] && ', ' + movieDetailsData.genres[1]?.name}
+                {movieDetailsData.genres[2] && ', ' + movieDetailsData.genres[2]?.name}
+              </td>
+            </tr>
+            <tr className="border-b border-gray-200 dark:border-gray-700">
+              <th scope="row" className="px-6 py-3 bg-gray-50 dark:bg-gray-800">
+                Overview
+              </th>
+              <td className="px-6 py-4">{movieDetailsData.overview}</td>
+            </tr>
+            <tr className="border-b border-gray-200 dark:border-gray-700">
+              <th scope="row" className="px-6 py-3 bg-gray-50 dark:bg-gray-800">
+                Production companies:
+              </th>
+              <td className="px-6 py-4">
+                {movieDetailsData.production_companies[0].name}
+                {movieDetailsData.genres[1] && ', ' + movieDetailsData.production_companies[1]?.name}
+              </td>
+            </tr>
+            <tr className="border-b border-gray-200 dark:border-gray-700">
+              <th scope="row" className="px-6 py-3 bg-gray-50 dark:bg-gray-800">
+                Release Date:
+              </th>
+              <td className="px-6 py-4">{movieDetailsData.release_date}</td>
+            </tr>
+            <tr className="border-b border-gray-200 dark:border-gray-700">
+              <th scope="row" className="px-6 py-3 bg-gray-50 dark:bg-gray-800">
+                Revenue:
+              </th>
+              <td className="px-6 py-4">{USDollar.format(movieDetailsData.revenue)}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </>
+  )
+}
--- app/(site)/movies/[movieId]/page.tsx
+++ app/(site)/movies/[movieId]/page.tsx
@@ -1,7 +1,30 @@
 import React from 'react'
 
-const Page = async ({ params }: { params: { movieId: string } }) => {
-  return <p>{params.movieId}</p>
+import { MovieDetails } from '@app/(site)/movies/[movieId]/MovieDetails'
+
+async function getData({ movieId }: { movieId: string }) {
+  const res = await fetch(`${process.env.MOVIES_DB_API_URL}/movie/${movieId}?api_key=${process.env.MOVIES_DB_API_KEY}`)
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch data')
+  }
+
+  return res.json()
 }
 
-export default Page
+export default async function Page({ params }: { params: { movieId: string } }) {
+  const imgURL = process.env.MOVIES_DB_API_IMAGE_URL!
+  const MOVIES_DB_API_URL = process.env.MOVIES_DB_API_URL!
+  const MOVIES_DB_API_KEY = process.env.MOVIES_DB_API_KEY!
+
+  const data = await getData({ movieId: params.movieId })
+
+  return (
+    <MovieDetails
+      results={data}
+      imgURL={imgURL}
+      MOVIES_DB_API_URL={MOVIES_DB_API_URL}
+      MOVIES_DB_API_KEY={MOVIES_DB_API_KEY}
+    ></MovieDetails>
+  )
+}
