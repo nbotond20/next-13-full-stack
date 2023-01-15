2e9d5de93dc282e4443517d1975c1d68ba673a68
Nuszpl Botond <nuszplbotond@gmail.com>
2023-01-15 13:49:52 +0100

    Fixing build error

--- app/(site)/movies/[movieId]/page.tsx
+++ app/(site)/movies/[movieId]/page.tsx
@@ -2,8 +2,16 @@ import React from 'react'
 
 import { MovieDetails, MovieDetailsProps } from '@app/(site)/movies/[movieId]/MovieDetails'
 
-async function getData({ movieId }: { movieId: string }) {
-  const res = await fetch(`${process.env.MOVIES_DB_API_URL!}movie/${movieId}?api_key=${process.env.MOVIES_DB_API_KEY!}`)
+async function getData({
+  movieId,
+  MOVIES_DB_API_URL,
+  MOVIES_DB_API_KEY,
+}: {
+  movieId: string
+  MOVIES_DB_API_URL: string
+  MOVIES_DB_API_KEY: string
+}) {
+  const res = await fetch(`${MOVIES_DB_API_URL}movie/${movieId}?api_key=${MOVIES_DB_API_KEY}`)
 
   if (!res.ok) {
     throw new Error('Failed to fetch data')
@@ -14,8 +22,10 @@ async function getData({ movieId }: { movieId: string }) {
 
 export default async function Page({ params }: { params: { movieId: string } }) {
   const imgURL = process.env.MOVIES_DB_API_IMAGE_URL!
+  const MOVIES_DB_API_URL = process.env.MOVIES_DB_API_URL!
+  const MOVIES_DB_API_KEY = process.env.MOVIES_DB_API_KEY!
 
-  const data = await getData({ movieId: params.movieId })
+  const data = await getData({ movieId: params.movieId, MOVIES_DB_API_URL, MOVIES_DB_API_KEY })
   const movieDetails: MovieDetailsProps = {
     results: data,
     imgURL: imgURL,
