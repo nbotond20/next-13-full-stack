20abc141964aea27aa3ebb66c374ffed21e9d14c
Nuszpl Botond <nuszplbotond@gmail.com>
2023-01-17 19:02:13 +0100

    Added comments

--- lib/utils/hashPass.ts
+++ lib/utils/hashPass.ts
@@ -1,9 +1,11 @@
 import bcrypt from 'bcrypt'
 
+// Hash password
 export const hashPass = async (password: string) => {
   return bcrypt.hash(password, 10).then(hash => hash)
 }
 
+// Compare password
 export const comparePass = async (password: string, hash: string) => {
   return bcrypt.compare(password, hash).then(res => res)
 }
